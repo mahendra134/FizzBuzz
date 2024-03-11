d36a408fe6612edeaaba49dc300aae503b41f698
mahendra134 <51395762+mahendra134@users.noreply.github.com>
2024-03-11 16:10:49 +0530

    Add files via upload

---
+++ FizzBuzz/Controllers/FizzBuzzController.cs
@@ -0,0 +1,33 @@
+﻿using FizzBuzz.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FizzBuzz.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FizzBuzzController : ControllerBase
+    {
+        private readonly IFizzBuzzService _fizzBuzzService;
+
+        public FizzBuzzController(IFizzBuzzService fizzBuzzService)
+        {
+            _fizzBuzzService = fizzBuzzService;
+        }
+
+        [HttpGet]
+        public ActionResult<string[]> GetFizzBuzzArray([FromQuery] string[] values)
+        {
+            try
+            {
+                var fizzBuzzArray = _fizzBuzzService.GetFizzBuzzArray(values);
+                return Ok(fizzBuzzArray);
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+    }
+}
